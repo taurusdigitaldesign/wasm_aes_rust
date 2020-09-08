b2e52bd9ac6c4a7c7285ac801e71263179ab909b
taurusdigitaldesign <taurusdigitaldesign@gmail.com>
2020-09-08 13:00:07 +0800

    fix: entry use string data which does not base64

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import * as wasm from "./wasm/pkg/wasm_aes";
 console.log(wasm.zx_encrypt('eyJhIjoiYiIsImIiOiJjIn0=', 'axb2c3e4f5$6e7%8', 'a1b2c3d4e5f6g7h8'));
 // console.log(wasm.zx_decrypt('8r4Glof5YVtt96nKrPSprw==', 'axb2c3e4f5$6e7%8', 'a1b2c3d4e5f6g7h8'));
 
-console.log(wasm.zx_encrypt('eyJwaG9uZSI6IjE4MDEyMzQ2NjY2IiwicGFzc3dvcmQiOiJhYWExMTEifQ==', '95796646b46fca55', '5796646b46fca551'));
+console.log(wasm.zx_encrypt('{"phone":"18012346666","password":"aaa111"}', '95796646b46fca55', '5796646b46fca551'));
